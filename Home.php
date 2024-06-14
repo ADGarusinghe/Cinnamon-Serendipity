af692daf5bee3a58a381cd4dd485714a1fa8b311
Amindi Garusinghe <amindigarusinghe2000@gmail.com>
2024-06-15 03:20:15 +0530

    2nd

--- Home.php
+++ Home.php
@@ -14,7 +14,7 @@
      <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
+<link rel="stylesheet" href="style.css">
 
 </head>
 <body>
@@ -23,12 +23,14 @@
 <section class="header">
   <a href="Home.php" class="logo">Cinnamon Serendipity</a>
 
+  <div class="gap"></div>
+  
   <nav class="navbar">
     <a href="Home.php">Home</a>
-    <a href="Home.php">About Us</a>
-    <a href="Home.php">Products</a>
-    <a href="Home.php">Cart</a>
-    <a href="Home.php">Cart</a>
+    <a href="About.php">About Us</a>
+    <a href="Products.php">Products</a>
+    <a href="Cart.php">Cart</a>
+    <a href="Purchase.php">Your Items</a>
   </nav>
 </section>
 
