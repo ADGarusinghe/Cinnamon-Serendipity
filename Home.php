1ac7505ab3ac45a043f0c20b5f6476de6073b3c9
Amindi Garusinghe <amindigarusinghe2000@gmail.com>
2024-06-15 15:59:25 +0530

    5th

--- Home.php
+++ Home.php
@@ -10,6 +10,9 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
      <link rel="stylesheet" href="css/style.css">
 
+     <!-- bootstrap -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
      <!-- poppins -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -36,11 +39,25 @@
 
 <div class="image">
 
-<img src="Cinnamon img\8.png" alt="bg">
+<!-- <img src="Cinnamon img\8.png" alt="bg"> -->
+
+<div class="carouselslide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Cinnamon img\8.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Cinnamon img\17.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Cinnamon img\16.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
 
 <div class="explore">
-<p class="para1">What do you know about world's best cinnamon products? <br> <b>Come explore with us</b> </p>
-<button class="explorebtn">Explore</button>
+<p class="para1">What do you know about world's best cinnamon products? <br> <span class="overbtn" ><b>Come explore with us</b></span> </p>
+<button class="explorebtn"> <a href="Products.php">Explore</a> </button>
 </div>
 
 
@@ -73,8 +90,10 @@
 
 
 
-
-
+<!-- bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
 <!-- swiper js -->
